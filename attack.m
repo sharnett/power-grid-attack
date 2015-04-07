60937572cc2086aff5ed7ee5d498fcd4497cf4ad
Sean Harnett <srharnett@gmail.com>
2015-04-07 16:01:16 -0400

    added 118 directory

--- attack.m
+++ attack.m
@@ -1,8 +1,9 @@
-function [result] = attack(mpc, x)
+function [result] = attack(mpc, x, alg)
     result = mpc;
     result.branch(:, 3:4) = [x x] .* result.branch(:, 3:4);
-    alg = 545; % try 545 (scpdipmopf) or 550 (tralmopf)
+    if (nargin < 3),
+        alg = 545; % try 545 (scpdipmopf) or 550 (tralmopf)
+    end
     opt = mpoption('OUT_ALL', 0, 'VERBOSE', 0, 'OPF_ALG', alg);
-    %opt = mpoption('OUT_ALL', 0, 'VERBOSE', 0);
     result = runopf(result, opt);
 end
---
+++ case118_example/enumerate118.m
@@ -0,0 +1,52 @@
+format short g;
+format compact;
+%%
+
+mpc = 'case118.m';
+subset = 1:186;
+
+%%
+clear m;
+m.mpc = mpc;
+m.subset = subset;
+m.gamma_max = 2.5;
+m.K = 3;
+m.Lnorm = 1;
+m.obj_type = 'angle';
+m.opf_obj_mult = 0;
+
+mpc = loadcase3(m.mpc);
+n = length(m.subset);
+o = ones(n, 1);
+
+attacks = nchoosek(1:n, 3);
+attacks = attacks(randperm(size(attacks, 1)), :);
+
+attacks = attacks
+%%
+sing_wrn = 'MATLAB:nearlySingularMatrix';
+warning('off', sing_wrn);
+
+for a=attacks',
+    g = zeros(n, 1);
+    g(a) = m.gamma_max;
+    
+    lastwarn('', '');
+    result = attack(mpc, o+g);
+    [~, msgidlast] = lastwarn;
+    
+    lastwarn('', '');
+    if (strcmp(msgidlast, sing_wrn)),
+        result = attack(mpc, o+g, 550);
+    end
+    [~, msgidlast] = lastwarn;
+    if (strcmp(msgidlast, sing_wrn)),
+        disp('singular even with trust region');
+    end
+    
+    f = objective(result, m.obj_type, m.Lnorm);
+    f2 = objective(result, 'magnitude', m.Lnorm);
+    disp([f f2 a']);
+end
+
+warning('on', sing_wrn);
